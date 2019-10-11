6c3bab34b5cb6b441a43668d498ac65aa3e79387
eric-greencomb <ministor@126.com>
2019-10-11 16:05:16 +0800

    c.

--- crypto/pbkdf2/hash_test.go
+++ crypto/pbkdf2/hash_test.go
@@ -32,10 +32,10 @@ func TestHash(t *testing.T) {
 
 func TestCheckHash(t *testing.T) {
 
-	_unique := Hash("13810167616", "f5d3c7fa", 16)
+	_unique := Hash("13810167616", "f5d3c7fa", 12)
 	fmt.Println(_unique)
 
-	_hash := "PBKI7KFGKAXXVLQCPQ7TKKUSNEVDQ"
+	_hash := "PBKI7KFGKAXXVLQCPQ7TKKQ"
 	fmt.Println(_hash[:3], _hash[3:])
 	if _hash[:3] == "PBK" {
 		if _unique != _hash[3:] {
