1291ef6265f5cd7b65f0c6351b063a915dd9549b
eric-greencomb <ministor@126.com>
2019-11-08 13:31:43 +0800

    c.

--- net/http/http_post.go
+++ net/http/http_post.go
@@ -7,8 +7,11 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
+	"net/http"
 	httpbase "net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -112,6 +115,52 @@ func PostForm(url string, data url.Values) (response string, err error) {
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+// PostFormTimeout 通过client的Do方法执行
+func PostFormTimeout(url string, data url.Values) (string, error) {
+	/*     生成client,参数默认;
+	*    这个结构体有四个属性
+	*    Transport(RoundTrepper);指定执行的独立、单次http请求的机制
+	*    CheckRedirect(func(req *Request, via []*Request)err):指定处理重定向的策略,如果不为nil,客户端在执行重定向之前调用本函数字段.如果CheckRedirect 返回一个错误，本类型中的get方法不会发送请求,如果CheckRedirect为nil,就会采用默认策略:连续请求10次后停止；
+	＊    jar(CookieJar):jar指定cookie管理器,若果为nil请求中不会发送cookie,回复中的cookie会被忽略
+	＊    TimeOut(time.Duration):指定本类型请求的时间限制，为0表示不设置超时
+	*/
+	//client := &http.Client{}    这里初始化了一个默认的client，并没有配置一些请求的设置
+
+	//可以通过client中transport的Dail函数,在自定义Dail函数里面设置建立连接超时时长和发送接受数据超时
+	_client := &http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				conn, err := net.DialTimeout(netw, addr, time.Second*3) //设置建立连接超时
+				if err != nil {
+					return nil, err
+				}
+				conn.SetDeadline(time.Now().Add(time.Second * 2)) //设置发送接受数据超时
+				return conn, nil
+			},
+			ResponseHeaderTimeout: time.Second * 2,
+		},
+	}
+	_reqest, err := http.NewRequest("POST", url, strings.NewReader(data.Encode())) //提交请求;用指定的方法，网址，可选的主体放回一个新的*Request
+	_reqest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+	_response, err := _client.Do(_reqest) //前面预处理一些参数，状态，Do执行发送；处理返回结果;Do:发送请求,
+	if err != nil {
+		return "", err
+	}
+
+	defer _response.Body.Close()
+
+	body, err := ioutil.ReadAll(_response.Body)
 	if err != nil {
 		return "", err
 	}
--- net/http/http_post_test.go
+++ net/http/http_post_test.go
@@ -1,9 +1,11 @@
 package http
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestPostForm(t *testing.T) {
@@ -17,6 +19,54 @@ func TestPostForm(t *testing.T) {
 	if _ok != "OK" {
 		t.Errorf("PostForm error")
 	}
+}
+
+func TestPostFormTimeout(t *testing.T) {
+
+	urlStr := "https://baidu.com"
+	form := make(url.Values)
+	form.Set("email", "ministor@126.com")
+	form.Set("pwd", "a11111")
+	_string := form.Encode()
+	fmt.Println(_string)
+	_ok, err := httpPostThree(urlStr, form)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(_ok)
+	}
+}
+
+func httpPostThree(url string, data url.Values) (string, error) {
+
+	fmt.Println(url)
+
+	fmt.Println("post first")
+	resp, err := PostFormTimeout(url, data)
+	if err == nil {
+		return resp, nil
+	}
+	fmt.Println("httpPostThree 1", err.Error())
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("post second")
+	resp, err = PostFormTimeout(url, data)
+	if err == nil {
+		return resp, nil
+	}
+	fmt.Println("httpPostThree 2", err.Error())
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("post three")
+	resp, err = PostFormTimeout(url, data)
+	if err == nil {
+		return resp, nil
+	}
+
+	fmt.Println("httpPostThree 3", err.Error())
+	return "", err
 }
 
 func TestPost(t *testing.T) {
